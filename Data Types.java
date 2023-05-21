247569cf1ae6e7d9b76fd368f13b900dcb321592
karishmashaiks <127649444+karishmashaiks@users.noreply.github.com>
2023-05-21 18:35:15 +0530

    Add files via upload

---
+++ Data Types.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int suitcase1 = 1000;
+        int suitcase2 = 2000;
+        int suitcase3 = 3000;
+        System.out.println(suitcase1+suitcase2+suitcase3);
+        System.out.print(suitcase1);
+        System.out.print(suitcase2);
+        System.out.print("suitcase3");
+    }
+}
