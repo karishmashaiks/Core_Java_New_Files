26e4a6437510473bafa4a63be4098c10602ba727
karishmashaiks <127649444+karishmashaiks@users.noreply.github.com>
2023-05-21 19:04:14 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String s="Karishma";
+        System.out.println(s.substring(6));
+        System.out.println(s.substring(0,6));
+    }
+}
