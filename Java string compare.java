f3759e3c55e1a0f472722caf1587aaadffcad450
karishmashaiks <127649444+karishmashaiks@users.noreply.github.com>
2023-05-21 18:37:36 +0530

    Add files via upload

---
+++ Java string compare.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String s1="Telugu";
+
+        String s2="Telugu";
+
+        String s3=new String("Telugu");
+
+
+        System.out.println(s1==s2);
+
+        System.out.println(s1==s3);
+
+
+    }
+}
