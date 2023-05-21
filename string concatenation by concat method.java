021c41372812fd8e203f27be98798f1275e46cab
karishmashaiks <127649444+karishmashaiks@users.noreply.github.com>
2023-05-21 18:58:52 +0530

    Add files via upload

---
+++ string concatenation by concat method.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String s1="Telugu";
+        String s2="Karishma";
+        String s3=s1.concat(s2);
+        System.out.println(s3);
+    }
+}
