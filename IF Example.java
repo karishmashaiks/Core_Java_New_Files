1afc9249caefb651015939445e5acbd416485a2d
karishmashaiks <127649444+karishmashaiks@users.noreply.github.com>
2023-05-23 09:32:28 +0530

    Add files via upload

---
+++ IF Example.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 16;
+        if (age > 18) {
+            System.out.println("He is allowed to vote");
+        }
+System.out.println("Sorry!");
+    }
+    }
