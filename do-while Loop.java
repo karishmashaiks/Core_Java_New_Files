9d2327184f7fc97b4dff0fe2c528f80386eaf0e7
karishmashaiks <127649444+karishmashaiks@users.noreply.github.com>
2023-05-23 09:02:23 +0530

    Add files via upload

---
+++ do-while Loop.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //code of java do While Loop
+        int i=1;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
